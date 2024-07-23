864d0d4835dcda9b05c0f9a90dd9d855482360e0
Deeksha224 <agarwaldeeksha224@gmail.com>
2024-07-24 00:15:47 +0530

    Time: 1 ms (76.47%), Space: 41 MB (25.61%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int reverse(int x) {
+        long temp = 0;
+        int r = Math.abs(x);
+        while (r>0){
+            int c = r % 10;
+            temp = temp * 10 + c;
+            r /= 10;
+        }
+        if (temp > 2147483646 ){
+            return 0;
+        }
+        if (x < 0){
+            temp = temp * -1;
+        }
+        int temp1 = (int) temp;
+        return temp1;
+    }
+}
