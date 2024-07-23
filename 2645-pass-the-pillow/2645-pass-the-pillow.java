86e0cee2e4d85805134bf0b3e1ed806585b74e20
Deeksha224 <agarwaldeeksha224@gmail.com>
2024-07-24 00:35:22 +0530

    Time: 0 ms (100%), Space: 40.3 MB (48.73%) - LeetHub

---
+++ 2645-pass-the-pillow/2645-pass-the-pillow.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int passThePillow(int n, int time) {
+        int complete = time / (n - 1);
+        int remain = time % (n - 1);
+        int ans = 0;
+        if (complete % 2 == 0){
+            ans = remain + 1;
+        }
+        else {
+            ans = n - remain;
+        }
+        return ans;
+    }
+}
