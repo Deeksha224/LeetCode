9899566d4dd23d6f69dce7a479e23deb1ad1e1d3
Deeksha224 <agarwaldeeksha224@gmail.com>
2024-07-24 00:37:19 +0530

    Time: 0 ms (100%), Space: 56.3 MB (39.22%) - LeetHub

---
+++ 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        // int start = 0, temp = Integer.MIN_VALUE;
+        // int i = 0;
+        // while ( i < arr.length){
+        //     temp = arr[i];
+        //     if (temp > arr[i + 1]){
+        //         start = i;
+        //         break;
+        //     }
+        //     i++;
+        // }
+        // return start;
+
+        // OR
+
+         int start = 0;
+        int end = arr.length - 1;
+        while(start < end){
+            int mid = start + (end - start) / 2;
+            if (arr[mid] < arr[mid + 1]){
+                start = mid + 1;
+            }
+            else{
+                end = mid;
+            }
+        }
+        return start;
+    }
+}
