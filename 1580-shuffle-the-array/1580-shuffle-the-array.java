2f497872791059ec9fcb75969dc72837a99ac286
Deeksha224 <agarwaldeeksha224@gmail.com>
2024-07-24 00:21:40 +0530

    Time: 0 ms (100%), Space: 44.3 MB (90.33%) - LeetHub

---
+++ 1580-shuffle-the-array/1580-shuffle-the-array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+       int[] result = new int [nums.length];
+       int index  = 0;
+       int arr1 = 0;
+       int arr2 = n;
+       while (arr2 != 2*n){
+            result[index++] = nums[arr1];
+            result[index++] = nums[arr2];
+            arr1++;
+            arr2++;
+        } 
+    return result;
+    }
+}
