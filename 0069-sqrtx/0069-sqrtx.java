d0fe392f8a45b4bb66d4eac65b946af4802b1d2c
Deeksha224 <agarwaldeeksha224@gmail.com>
2024-10-14 07:57:28 +0530

    Time: 1 ms (85.62%), Space: 41.1 MB (13.98%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,6 +1,20 @@
 class Solution {
     public int mySqrt(int x) {
-        int y = (int)Math.sqrt(x);
-        return y;
+        long start = 0;
+        long end = x;
+        long mid = 0;
+        while (start <= end){
+            mid = start + (end - start) / 2;
+            if (mid * mid > x){
+                end = mid - 1;
+            }
+            else if (mid * mid == x){
+                return (int)mid;
+            }
+            else {
+                start = mid + 1;
+            }
+        }
+        return (int)end ;
     }
 }
