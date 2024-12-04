0ef1265ec076be05669eeef39ca0467a71744fb6
Deeksha224 <agarwaldeeksha224@gmail.com>
2024-12-04 23:13:22 +0530

    Time: 8 ms (5.23%), Space: 42.5 MB (5.26%) - LeetHub

---
+++ 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        char[] arr = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+        char[] sen = sentence.toCharArray();
+        Arrays.sort(sen);
+        Arrays.sort(arr);
+        String str = "";
+        str += sen[0];
+        for (int i = 1; i < sen.length; i++){
+            if (sen[i] != sen[i - 1]){
+                str += sen[i];
+            }
+        }
+        char[] sen1 = str.toCharArray();
+        return (Arrays.equals(sen1,arr));
+    }
+}
