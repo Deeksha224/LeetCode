6e5c2491d5b6de4a63f14578351e13922b40aedb
Deeksha224 <agarwaldeeksha224@gmail.com>
2025-04-24 15:42:44 +0530

    Time: 1 ms (27.89%), Space: 44.8 MB (49.9%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -13,22 +13,48 @@
  *     }
  * }
  */
+
+ // the complexity of code is n^2 
+
+// class Solution {
+//     public int diameterOfBinaryTree(TreeNode root) {
+//         if (root == null){
+//             return -1;
+//         }
+//         int ld = diameterOfBinaryTree(root.left);
+//         int rd = diameterOfBinaryTree(root.right);
+//         int sd = ht(root.left)+ht(root.right) + 2;
+//         return Math.max(sd,Math.max(rd,ld));
+//     }
+//     public int ht(TreeNode root){
+//         if (root==null){
+//             return -1;
+//         }
+//         int lh = ht(root.left);
+//         int rg = ht(root.right);
+//         return Math.max(lh,rg) + 1;
+//     }
+// }
+
 class Solution {
     public int diameterOfBinaryTree(TreeNode root) {
-        if (root == null){
-            return -1;
-        }
-        int ld = diameterOfBinaryTree(root.left);
-        int rd = diameterOfBinaryTree(root.right);
-        int sd = ht(root.left)+ht(root.right) + 2;
-        return Math.max(sd,Math.max(rd,ld));
+        return diameter(root).dt;
     }
-    public int ht(TreeNode root){
-        if (root==null){
-            return -1;
+    public DiaPair diameter(TreeNode root) {
+        if (root == null){
+            return new DiaPair();
         }
-        int lh = ht(root.left);
-        int rg = ht(root.right);
-        return Math.max(lh,rg) + 1;
+        DiaPair ldp = diameter(root.left);  // dt ht
+        DiaPair rdp = diameter(root.right);  // dt ht
+        int sd = ldp.ht + rdp.ht + 2;
+        DiaPair sdp = new DiaPair();
+        sdp.dt = Math.max(sd,Math.max(rdp.dt, ldp.dt));
+        sdp.ht = Math.max(ldp.ht,rdp.ht) + 1;
+        return sdp;
+    }
+    class DiaPair{
+        int dt = 0;
+        int ht = -1;
+
     }
 }
