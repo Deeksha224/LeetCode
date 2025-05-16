5640004eb5c9e187c682e9f1c88577a0a842d144
Deeksha224 <agarwaldeeksha224@gmail.com>
2025-05-17 00:29:43 +0530

    Time: 0 ms (100%), Space: 44.3 MB (95.69%) - LeetHub

---
+++ 0861-flipping-an-image/0861-flipping-an-image.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[][] flipAndInvertImage(int[][] image) {
+        int n = image.length;
+        for (int[] row : image){
+            // reverse the array
+            for (int i = 0; i < (n + 1)/2; i++){
+                // swapping the first and last while taking Xor for changing the digit
+                int temp = row[i] ^ 1;
+                row[i] = row[(n - i - 1)] ^ 1;
+                row[(n-i-1)] = temp;
+            }
+        }
+        return image;
+    }
+}
