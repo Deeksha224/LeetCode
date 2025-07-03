d0150a0d99f13b4672ae3b67369b0d8accdbdfae
Deeksha Agarwal <agarwaldeeksha224@gmail.com>
2025-07-04 00:55:39 +0530

    Time: 11 ms (16.54%), Space: 42.2 MB (19.25%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -0,0 +1,21 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        HashMap<Character,Integer> freq = new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            freq.put(ch,freq.getOrDefault(ch,0)+1);
+        }
+        HashMap<Character,Integer> freq1= new HashMap<>();
+        for(int i=0;i<t.length();i++){
+            char ch=t.charAt(i);
+            freq1.put(ch,freq1.getOrDefault(ch,0)+1);
+        }
+        for(int i=0;i<t.length();i++){
+            char ch=t.charAt(i);
+           if(freq1.get(ch)!=freq.get(ch)){
+                return ch;
+            }
+        }
+        return 'a';
+    }
+}
